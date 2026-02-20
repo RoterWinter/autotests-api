79a9094f91990d24b6f39dd646c49067eb758f3c
RoterWinter <alenakapusova@gmail.com>
2026-02-20 16:55:19 +0500

    Знакомство с Pydantic

---
+++ pydantic_create_user.py
@@ -0,0 +1,27 @@
+import uuid
+
+from pydantic import BaseModel, Field, EmailStr, HttpUrl, ValidationError
+
+class UserSchema(BaseModel):
+    id: str
+    email: EmailStr  # Используем EmailStr вместо str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserSchema
