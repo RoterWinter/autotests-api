eeb6f5bb4750b4cb140dc03884f27222cdcb0be1
RoterWinter <alenakapusova@gmail.com>
2026-02-24 10:45:41 +0500

    Практика написания API автотестов

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -29,3 +29,18 @@ def assert_equal(actual: Any, expected: Any, name: str):
         f'Expected value: {expected}. '
         f'Actual value: {actual}'
     )
+
+# Остальной код без изменений
+
+def assert_is_true(actual: Any, name: str):
+    """
+    Проверяет, что фактическое значение является истинным.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :raises AssertionError: Если фактическое значение ложно.
+    """
+    assert actual, (
+        f'Incorrect value: "{name}". '
+        f'Expected true value but got: {actual}'
+    )
