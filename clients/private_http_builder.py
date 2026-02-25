e87320634b9047ab75d3fb1074d8feaa590b351f
RoterWinter <alenakapusova@gmail.com>
2026-02-25 15:20:05 +0500

    cURL в Allure-отчете: упрощаем анализ API-тестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, ConfigDict
 from clients.authentication.authentication_client import get_authentication_client
 # Импортируем модель LoginRequestSchema
 from clients.authentication.authentication_schema import LoginRequestSchema
+from clients.event_hooks import curl_event_hook
 
 
 # Добавили суффикс Schema вместо Dict
@@ -16,16 +17,20 @@ class AuthenticationUserSchema(BaseModel):  # Наследуем от BaseModel 
 
 @lru_cache(maxsize=None)
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
+    """
+    Функция создаёт экземпляр httpx.Client с аутентификацией пользователя.
+
+    :param user: Объект AuthenticationUserSchema с email и паролем пользователя.
+    :return: Готовый к использованию объект httpx.Client с установленным заголовком Authorization.
+    """
     authentication_client = get_authentication_client()
 
-    # Используем модель LoginRequestSchema
-    # Значения теперь извлекаем не по ключу, а через атрибуты
     login_request = LoginRequestSchema(email=user.email, password=user.password)
     login_response = authentication_client.login(login_request)
 
     return Client(
         timeout=100,
         base_url="http://localhost:8000",
-        # Значения теперь извлекаем не по ключу, а через атрибуты
-        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"},
+        event_hooks={"request": [curl_event_hook]}  # Добавляем event hook для запроса
     )
