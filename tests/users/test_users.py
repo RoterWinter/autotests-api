91abea7432cbe54188f7f65f20fd50b6bfa85fcb
RoterWinter <alenakapusova@gmail.com>
2026-02-25 14:28:50 +0500

    Интеграция Allure в автотесты

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,11 +1,17 @@
 from http import HTTPStatus
 
+import allure
 import pytest
 
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from fixtures.users import UserFixture  # Заменяем импорт
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.severity import Severity
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
@@ -14,9 +20,17 @@ from tools.fakers import fake
 
 @pytest.mark.users
 @pytest.mark.regression
+@allure.tag(AllureTag.USERS, AllureTag.REGRESSION)
+@allure.epic(AllureEpic.LMS)  # Добавили epic
+@allure.feature(AllureFeature.USERS)  # Добавили feature
 class TestUsers:
     @pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
+    @allure.tag(AllureTag.CREATE_ENTITY)
+    @allure.story(AllureStory.CREATE_ENTITY)  # Добавили story
+    @allure.title("Create user")  # Добавляем человекочитаемый заголовок
+    @allure.severity(Severity.BLOCKER)
     def test_create_user(self, email: str, public_users_client: PublicUsersClient):
+        allure.dynamic.title(f"Attempt to create user with email: {email}")  # Заголовок через allure.dynamic.title
         request = CreateUserRequestSchema(email=fake.email(domain=email))
         response = public_users_client.create_user_api(request)
         response_data = CreateUserResponseSchema.model_validate_json(response.text)
@@ -27,7 +41,10 @@ class TestUsers:
 
         validate_json_schema(response.json(), response_data.model_json_schema())
 
-
+    @allure.tag(AllureTag.GET_ENTITY)  # Используем enum
+    @allure.title("Get user me")
+    @allure.story(AllureStory.GET_ENTITY)
+    @allure.severity(Severity.CRITICAL)
     def test_get_user_me(self, function_user: UserFixture, private_users_client: PrivateUsersClient):
         response = private_users_client.get_user_me_api()
         response_data = GetUserResponseSchema.model_validate_json(response.text)
